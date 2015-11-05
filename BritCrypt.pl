0cafa900e73cd7e02c7ab1f89b59c2e3d653681f
David Davidson <g00271860@gmit.ie>
2015-11-05 18:54:06 +0000

    David Cameron Fucks Pigs. Tru story.

---
+++ BritCrypt.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+# you will need: http://search.cpan.org/CPAN/authors/id/A/AY/AYRNIEU/Crypt-Rot13-0.6.tar.gz
+# ~skyhighatrist (2015)
+use strict;
+use warnings;
+use Crypt::Rot13;
+use Term::ANSIColor;
+# defining the colours here as variables to use later :)
+my $green = color('bold bright_green');
+my $blue = color('bold bright_blue'); 
+my $cyan = color('bold bright_cyan');
+my $red = color('bold bright_red');
+my $magenta = color('bold bright_magenta');
+my $reset = color('reset');
+
+# yeah, functioney
+sub BritCrypt {
+    my ($infile, $outfile) = @_; # we just take the first two args.
+    print $cyan."[*] ".$green."Reading $infile... Do tell us all your secrets!$reset\n";
+    my $supersecureencryptionalgorithm = new Crypt::Rot13;
+    open(THERESA_MAY, $infile) or die "$!";
+    print $magenta."[+] ".$blue."Preparing the encryption...$reset\n";
+    $supersecureencryptionalgorithm->charge(<THERESA_MAY>);
+    print $magenta."[+] ".$blue."Encryption charged with plaintext...$reset\n";
+    close(THERESA_MAY) or die "$!";
+    open(DAVID_CAMERON, '>', $outfile) or die "$!";
+    print $magenta."[+] ".$blue."Saving your secrets...$reset\n";
+    print DAVID_CAMERON $supersecureencryptionalgorithm->rot13(-13);
+    close(DAVID_CAMERON) or die "$!";
+    print $cyan."[*] ".$green."Saved ciphertext to $outfile, do make sure to forward a copy to GCHQ!$reset\n";
+}
+
+if ($#ARGV + 1 != 2){ # count yer args before.
+    print $red."BritCrypt - Completely Reversible, Theresa May Friendly Message Encryption Legal for Use in the UK\nUse: BritCrypt.pl input_plaintext.txt output_ciphertext.txt$reset\n";
+    exit;
+}
+BritCrypt($ARGV[0], $ARGV[1]); # call our function with our args
+#_EOF
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # BritCrypt
-Theresa May Approved Crypto for British Citizens
+*Theresa May Approved Crypto for British Citizens*
+
+## What?
+This tool is a message-encryption tool designed so that British citizens can encrypt their messages, and also comply with Theresa May's demands for prohibition on strong cryptography.
+
+## Why?
+I wanted to write some Perl, and help our dear friends in the United Kingdom move away from strong cryptography as gently as possible.
+
+## Deps
+Its written in Perl, but you will need the `Crypt::Rot13` module, which you can find [on the cpan website](http://search.cpan.org/~ayrnieu/Crypt-Rot13-0.6/Rot13.pm).
+
+## Screenshot
+
+## Licence
+Licenced under the [WTFPL](http://wtfpl.net)
